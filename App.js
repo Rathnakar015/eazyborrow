9207882d4a90362075f9863d0655f10c8c6198ab
Rathnakar015 <rathnakar005@gmail.com>
2021-03-17 12:12:35 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { firebase } from './src/firebase/config'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
-const App = () => {
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <></>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+        { user ? (
+          <Stack.Screen name="Home" >
+            {props => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-  },
-});
-
-export default App;
+}
